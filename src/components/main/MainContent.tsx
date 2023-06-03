4acdb31624ddfb8c88ebb4996cd57686a4362d03
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2023-06-03 20:02:10 -0300

    feat: add initial projects list structure

--- src/components/main/MainContent.tsx
+++ src/components/main/MainContent.tsx
@@ -5,18 +5,34 @@ import './styles.css'
 
 import beforeImage from '../../images/slider_before.jpg';
 import afterImage from '../../images/slider_before.jpg';
+import projectImage from '../../images/project.jpg';
 
 const MainContent: React.FC = () => {
   const mainElementRef = useRef<any>(null);
 
   return (
     <main ref={mainElementRef} className='min-h-screen'>
-      <ReactBeforeSliderComponent 
+      <ReactBeforeSliderComponent
         className='slider_content'
-        firstImage={{imageUrl: beforeImage}}
-        secondImage={{imageUrl: afterImage}}
-        />
-        <p>Resto do Conteúdo</p>
+        firstImage={{ imageUrl: beforeImage }}
+        secondImage={{ imageUrl: afterImage }}
+      />
+      <div className='projects_container flex justify-center w-100 mt-24 space-x-4'>
+        <div className='w-6/12 relative'>
+          <div className='project_label absolute top-8 left-8 z-40'>
+            <span className='project_title'>Project 01</span>
+            <span className='project_year'>2023</span>
+          </div>
+          <img src={projectImage} alt="Project 01 - 2023" />
+        </div>
+        <div className='w-6/12 relative'>
+          {/* <div className='project_label absolute inline'>
+            <span className='project_title'>Project 02</span>
+            <span className='project_year'>2023</span>
+          </div> */}
+          <img src={projectImage} alt="Project 02 - 2023" />
+        </div>
+      </div>
     </main>
   );
 }
