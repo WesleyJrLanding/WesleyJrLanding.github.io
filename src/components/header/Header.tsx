002e4ce259997aaaf3353efb7379d4bb01bfac7d
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2023-06-05 17:26:22 -0300

    feat: adjust header content to be responsible

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -16,12 +16,12 @@ const Header: React.FC = () => {
   const [openModal, setOpenModal] = useState<ModalsEnum | null>(null)
 
   return <header className='landing_page_header flex justify-center items-center flex-col min-h-screen'>
-    <h1 className='font-bold text-6xl'>Wesley Junior</h1>
-    <h2 className='font-semibold text-2xl mt-4'>Visual Effects Compositor | Motion Designer</h2>
+    <h1 className='font-bold md:text-6xl text-3xl'>Wesley Junior</h1>
+    <h2 className='font-semibold md:text-2xl mt-4 text-xs'>Visual Effects Compositor | Motion Designer</h2>
     <SocialMediaLinks/>
-    <div className='demo_buttons_container flex justify-center space-x-64 my-32'>
-      <button onClick={() =>setOpenModal(ModalsEnum.NUKE_DEMO)} className='demo_button rounded-lg font-bold py-2 px-4'>Nuke Demo Reel</button>
-      <button onClick={() =>setOpenModal(ModalsEnum.AFTER_EFFECTS)} className='demo_button rounded-lg font-bold py-2 px-4'>After Effects Demo Reel</button>
+    <div className='demo_buttons_container flex justify-center space-x-2 md:space-x-64 md:my-32 my-10'>
+      <button onClick={() =>setOpenModal(ModalsEnum.NUKE_DEMO)} className='demo_button rounded-lg font-semibold md:font-bold md:py-2 md:px-4 text-sm md:text-base'>Nuke Demo Reel</button>
+      <button onClick={() =>setOpenModal(ModalsEnum.AFTER_EFFECTS)} className='demo_button rounded-lg font-semibold md:font-bold md:py-2 md:px-4 text-sm md:text-base'>After Effects Demo Reel</button>
     </div>
     <DemoModal 
       isOpen={openModal === ModalsEnum.NUKE_DEMO} 
--- src/components/main-container/MainContainer.tsx
+++ src/components/main-container/MainContainer.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from 'react';
 import './style.css';
 
 const MainContainer: React.FC<{children: ReactNode}> = ({ children }) => {
-    return <div className='main_container px-12 py-24'>
+    return <div className='main_container overflow-x-hidden md:px-12 md:py-24 px-6 py-12'>
         {children}
     </div>
 }
